96a8fc50f51e33faef97c2b8bb257691ca0bdba1
Jister <chenjiesjtu14@gmail.com>
2016-01-16 17:46:53 +0800

    update

---
+++ src/lidarlite.cpp
@@ -0,0 +1,36 @@
+#include "ros/ros.h"
+#include "lidarLite.h"
+#include "lidar_lite_ros/Lidarlite.h"
+#include "std_msgs/String.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "lidarlite_node");
+  ros::NodeHandle n;
+  ros::Publisher lidar_pub = n.advertise<lidar_lite_ros::Lidarlite>("lidar_distance", 1000);
+  ros::Rate loop_rate(10);
+
+  int fd;
+  int value;
+  unsigned char status;
+  lidar_lite_ros::Lidarlite msg;
+
+  fd = lidar_init(false);
+  if (fd == -1) {
+    printf("initialization error\n");
+  }
+  while (ros::ok())
+  {
+    value = lidar_read(fd);
+    status = lidar_status(fd);
+    ROS_INFO("%3.0d cm", value);
+    msg.distance = value;
+    lidar_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
